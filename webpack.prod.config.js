7948542ef3468e11721a9597ca7833d8811dffb9
Ken Ding <aenkil@gmail.com>
2016-02-27 19:10:18 +1100

    Add webpack compile step to a dist folder

---
+++ webpack.prod.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel',
+        exclude: path.join(__dirname, '/node_modules')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ]
+};
+
